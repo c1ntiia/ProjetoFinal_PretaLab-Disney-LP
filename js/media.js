448397c4c3ad82c67fca62ea8f1e23cc3e73251f
c1ntiia <123259829+c1ntiia@users.noreply.github.com>
2023-03-17 11:56:54 -0300

    Update media.js

--- js/media.js
+++ js/media.js
@@ -19,11 +19,24 @@ function calcularMedia() {
 
   let mensagem = "";
   // evite mexer no código acima!
-
-
-
-
-
+const media = (nota1 + note2 + note3 + note4) / 4;
+  
   // evite mexer na linha de código abaixo!
   document.getElementById("situacaoAluno").innerHTML = mensagem;
+  
+  if(media==0){
+    console.log (Infelizmente você zerou a prova :c);
+} else(media=>0.1 && media<=3){
+    console.log (Caramba, deu ruim, você obteve media {media)! Estude mais e tente novamente);
+} else(media=>3.1 && media<=5.9){
+    console.log (Você obteve media {media} ! Falta pouco para a média.
+} else(media=6 && media==7){
+    console.log (Você está na média com {media});
+} else(media=>7.1 && media=<9.9){
+    console.log (Notão! Sua média é {media}!);
+} else(media==10){
+    console.log (Hoje é seu aniversário? Pq você ta de parabéns! 10 de média);
+} else(media>10){
+    console.log (As notas informadas estão erradas);
+}
 }
