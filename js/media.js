495f3d68569292fa30f22d62cb4e018a716c8d28
c1ntiia <123259829+c1ntiia@users.noreply.github.com>
2023-03-18 16:16:54 -0300

    Update media.js

--- js/media.js
+++ js/media.js
@@ -23,7 +23,7 @@ function calcularMedia() {
  
   const calcularMedia = (nota1 + note2 + note3 + note4) / 4;
  
-    if(calcularMedia==0){
+    if(calcularMedia<=0){
     alert = ("Infelizmente você zerou a prova :c");      
 }    else if(calcularMedia>0.1 && calcularMedia<3){
       alert ("Caramba, deu ruim, você obteve media " + calcularMedia + " Estude mais e tente novamente");
